a00e53e82e4a624df100fe8be180da78dbd7bbb0
Dzianis Andreyenka <andreenkodn@gmail.com>
2024-12-19 17:33:04 +0100

    Store a simple data
    Signed-off-by: Dzianis Andreyenka <andreenkodn@gmail.com>

--- programs/anchor-playground/src/lib.rs
+++ programs/anchor-playground/src/lib.rs
@@ -1,16 +1,36 @@
+// use this import to gain access to common anchor features
 use anchor_lang::prelude::*;
 
+// declare an id for your program
 declare_id!("ENWwGmKmN5quw638BYpeJhU7rhoVFpfDwCgRFwjv8ACD");
 
+// write your business logic here
 #[program]
 pub mod anchor_playground {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize(ctx: Context<Initialize>, data: u64) -> Result<()> {
+        ctx.accounts.new_account.data = data;
+        msg!("Changed data to: {}!", data); // Message will show up in the tx logs
         Ok(())
     }
 }
 
+// validate incoming accounts here
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    // We must specify the space in order to initialize an account.
+    // First 8 bytes are default account discriminator,
+    // next 8 bytes come from NewAccount.data being type u64.
+    // (u64 = 64 bits unsigned integer = 8 bytes)
+    #[account(init, payer = signer, space = 8 + 8)]
+    pub new_account: Account<'info, NewAccount>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct NewAccount {
+    data: u64,
+}
--- tests/anchor-playground.ts
+++ tests/anchor-playground.ts
@@ -1,16 +1,46 @@
 import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
+import { Program, BN } from "@coral-xyz/anchor";
 import { AnchorPlayground } from "../target/types/anchor_playground";
+import { Keypair, SystemProgram } from "@solana/web3.js";
+import assert from "assert";
 
 describe("anchor-playground", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  const wallet = provider.wallet as anchor.Wallet;
+  const connection = provider.connection;
 
   const program = anchor.workspace.AnchorPlayground as Program<AnchorPlayground>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("initialize", async () => {
+    // Generate keypair for the new account
+    const newAccountKp = new Keypair();
+
+    // Send transaction
+    const data = new BN(42);
+    const txHash = await program.methods
+      .initialize(data)
+      .accounts({
+        newAccount: newAccountKp.publicKey,
+        signer: wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      })
+      .signers([newAccountKp])
+      .rpc();
+    console.log(`Use 'solana confirm -v ${txHash}' to see the logs`);
+
+    // Confirm transaction
+    await connection.confirmTransaction(txHash);
+
+    // Fetch the created account
+    const newAccount = await program.account.newAccount.fetch(
+      newAccountKp.publicKey
+    );
+
+    console.log("On-chain data is:", newAccount.data.toString());
+
+    // Check whether the data on-chain is equal to local 'data'
+    assert(data.eq(newAccount.data));
   });
 });
